1ce5f3c5205fa325e081505b8fd9ec6237797ec4
dvazar <adv-tsk@yandex.ru>
2020-06-13 16:12:28 +0700

    Heap sorting algorithm

---
+++ src/heap_sort.rs
@@ -0,0 +1,57 @@
+/// Heap sorting algorithm, O(n log n)
+///
+/// Input: <a1, a2, ..., an>.
+/// Output: <a1', a2', ..., an'>, where a1' <= a2' <= ... <= an'.
+///
+
+fn heapify (array: &mut Vec<i32>, idx: usize, max: usize) {
+    let mut largest = idx;
+    let left = idx * 2 + 1;
+    let right = idx * 2 + 2;
+    if left < max && array[left] > array[largest] {
+        largest = left;
+    }
+    if right < max && array[right] > array[largest] {
+        largest = right;
+    }
+    if largest != idx {
+        let tmp = array[idx];
+        array[idx] = array[largest];
+        array[largest] = tmp;
+        heapify(array, largest, max);
+    }
+}
+
+fn build_heap (array: &mut Vec<i32>) {
+    for i in (0..array.len()/2-1).rev() {
+        heapify(array, i, array.len());
+    }
+}
+
+
+pub fn heap_sort (array: &mut Vec<i32>) {
+    build_heap(array);
+    for i in (1..array.len()).rev() {
+        let tmp = array[0];
+        array[0] = array[i];
+        array[i] = tmp;
+        heapify(array, 0, i);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_heap_sort() {
+        let mut array = vec![5, 2, 4, 6, 1, 3];
+        let expected_array = vec![1, 2, 3, 4, 5, 6];
+
+        heap_sort(&mut array);
+
+        assert_eq!(array, expected_array);
+    }
+
+}
